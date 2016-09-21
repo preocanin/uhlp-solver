8abc071f07f6b666b70d0e1f4edbbc70f55f576a
Aleksandar <preocanin.aleksandar@gmail.com>
2016-09-22 00:34:16 +0200

    Added private functions for calculating objective function

--- include/particle.h
+++ include/particle.h
@@ -7,14 +7,43 @@
 
 class Particle {
 	public:
-		Particle(const Parameter& param) 
-			: _param(param), _solution(param.getN(),-1) {}
+		Particle(const Parameter&,std::vector<int>&);
+
+		bool isEmpty() const {
+			return current_solution == std::vector<int>(_param.getN(),-1); 
+		}
+
+		bool operator== (const Particle& p) const {
+			return current_solution == p.current_solution;
+		}
+
+		bool operator< (const Particle& p) const {
+			return objectiveCurrent < p.objectiveCurrent;
+		}
+
+		bool operator> (const Particle& p) const {
+			return objectiveCurrent > p.objectiveCurrent;
+		}
+	
+			
 
 		~Particle() {}
 		
 	private:
 		const Parameter& _param;
-		std::vector<int> _solution;
+
+		double objectiveCurrent;
+		double objectiveLocalMin;
+
+		std::vector<int> current_solution;
+		std::vector<int> local_best;
+		std::vector<int> &global_best;
+		std::vector<int> vector_speed;
+
+		double calculateObjectiveFn() const;
+		double calculateFlowCost(const int i,const int j) const {
+			return _param.getCij(i,j)*_param.getWij(i,j); 
+		}
 };
 
 #endif
--- lib/particle.cpp
+++ lib/particle.cpp
@@ -1,3 +1,74 @@
 #include "../include/particle.h"
 
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <set>
+#include <cstdlib>
+
+Particle::Particle(const Parameter& param,std::vector<int>& best) 
+	: _param(param), current_solution(param.getN(),-1), global_best(best)
+{
+	for(unsigned i = 0; i <  current_solution.size() ; ++i) {
+		if(current_solution[i] == -1) {
+			int nodeHub = std::rand() % param.getN();
+			current_solution[i] = nodeHub;
+			current_solution[nodeHub] = nodeHub;	
+		}		
+	}
+
+	std::copy(current_solution.begin(),current_solution.end(),
+			std::back_inserter(local_best));
+
+	objectiveCurrent = calculateObjectiveFn();
+	objectiveLocalMin = objectiveCurrent;
+
+	for(int i = 0; i < param.getN() ; ++i) {
+		int zero = std::rand()%2;
+		int v = std::rand()%param.getN();
+		vector_speed.push_back((-1)*zero == 0?v:-v);
+	}
+
+}
+
+double Particle::calculateObjectiveFn() const {
+	double sumFcost = 0.0,sumFlowCost=0.0;
+	std::set<int> setOfHubs(current_solution.begin(),current_solution.end());
+	for(const unsigned& i : setOfHubs)
+		sumFcost += _param.getFi(i);
+
+	if(_param.format == FI_AP) {
+		double c = _param.getC();
+		double d = _param.getD();
+		double cik,ckm,cmj;
+
+		for (int i = 0; i < _param.getN() ; ++i) 
+			for (int j = 0;  j < _param.getN() ; ++j)  {
+				if(i!=j) {
+					cik = c*calculateFlowCost(i,current_solution[i]);
+					ckm =	_param.getAlpha() *calculateFlowCost(current_solution[i],
+							current_solution[j]);
+					cmj = d*calculateFlowCost(current_solution[j],j);
+					sumFlowCost += cik+ckm+cmj;
+				}
+
+			}
+	} else {
+		double sumFlowCost = 0.0;
+		double c1,c2,c3;
+
+		for(int i = 0; i < _param.getN() ; ++i)
+			for(int j = i; j < _param.getN() ; ++j) {
+				if(i != j) {
+					c1 = calculateFlowCost(i,current_solution[i]);
+					c2 = _param.getAlpha()*calculateFlowCost(current_solution[i],
+				                           current_solution[j]);
+					c3 = calculateFlowCost(current_solution[j],j);			
+					sumFlowCost += 2 * (c1 + c2 + c3);
+				}
+			}
+	}
+	return sumFlowCost+sumFcost;
+}
+
 
