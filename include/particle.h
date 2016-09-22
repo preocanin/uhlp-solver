6f916765013097ef0596f1ff815d5234f73ebf99
Aleksandar <preocanin.aleksandar@gmail.com>
2016-09-22 21:06:39 +0200

    Added changes to constructor and function move

--- include/particle.h
+++ include/particle.h
@@ -4,10 +4,13 @@
 
 #include "parameter.h"
 #include <vector>
+#include <map>
 
 class Particle {
 	public:
-		Particle(const Parameter&,std::vector<int>&);
+		Particle(const Parameter&);
+
+		void move(const double&,const double&,const double&);
 
 		bool isEmpty() const {
 			return current_solution == std::vector<int>(_param.getN(),-1); 
@@ -24,7 +27,8 @@ class Particle {
 		bool operator> (const Particle& p) const {
 			return objectiveCurrent > p.objectiveCurrent;
 		}
-	
+
+		double getGlobalObj() const { return objectiveGlobal; }	
 			
 
 		~Particle() {}
@@ -35,15 +39,18 @@ class Particle {
 		double objectiveCurrent;
 		double objectiveLocalMin;
 
+		double vmax,vmin;
+
 		std::vector<int> current_solution;
 		std::vector<int> local_best;
-		std::vector<int> &global_best;
 		std::vector<int> vector_speed;
 
+		static double objectiveGlobal;
+		static std::vector<int> global_best;
+		static std::map<std::vector<int>,double> all_solutions;
+
 		double calculateObjectiveFn() const;
-		double calculateFlowCost(const int i,const int j) const {
-			return _param.getCij(i,j)*_param.getWij(i,j); 
-		}
 };
+
 
 #endif
--- lib/particle.cpp
+++ lib/particle.cpp
@@ -5,10 +5,21 @@
 #include <algorithm>
 #include <set>
 #include <cstdlib>
+#include <climits>
+#include <cmath>
+#include <map>
 
-Particle::Particle(const Parameter& param,std::vector<int>& best) 
-	: _param(param), current_solution(param.getN(),-1), global_best(best)
+double Particle::objectiveGlobal = 0.0;
+std::vector<int> Particle::global_best;
+std::map<std::vector<int>,double> Particle::all_solutions;
+
+Particle::Particle(const Parameter& param) 
+	: _param(param), current_solution(param.getN(),-1)
 {
+	vmax = param.getN() - 1;
+	vmin = -vmax;
+
+	//Generating current solution
 	for(unsigned i = 0; i <  current_solution.size() ; ++i) {
 		if(current_solution[i] == -1) {
 			int nodeHub = std::rand() % param.getN();
@@ -17,57 +28,109 @@ Particle::Particle(const Parameter& param,std::vector<int>& best)
 		}		
 	}
 
+	//Local best is a current solution
 	std::copy(current_solution.begin(),current_solution.end(),
 			std::back_inserter(local_best));
 
+	//Local best objective is equal to current objective
 	objectiveCurrent = calculateObjectiveFn();
 	objectiveLocalMin = objectiveCurrent;
 
+	//Generating velocity vector
 	for(int i = 0; i < param.getN() ; ++i) {
 		int zero = std::rand()%2;
 		int v = std::rand()%param.getN();
 		vector_speed.push_back((-1)*zero == 0?v:-v);
 	}
 
+	//Updating the global best solution and his objective
+	if(global_best.size() == 0) {
+		global_best = current_solution;
+		objectiveGlobal = objectiveCurrent;
+	} else {
+		if(objectiveGlobal < objectiveCurrent) {
+			global_best = current_solution;
+			objectiveGlobal = objectiveCurrent;
+		}
+	}
+
+	//Add current solution to Map
+	if(all_solutions.find(current_solution) == all_solutions.end())
+		all_solutions[current_solution] = objectiveCurrent;
+
+}
+
+void Particle::move(const double& ul, const double& ug, const double& vel) {
+	
+	for(unsigned i = 0; i < current_solution.size() ; ++i) {
+		double fl = std::rand()/(1.0*INT_MAX);
+		double fg = std::rand()/(1.0*INT_MAX);
+	
+		double vtr = vel*vector_speed[i] +
+		   			 ul*fl*(local_best[i] - current_solution[i]) +
+				     ug*fg*(global_best[i] - current_solution[i]);
+
+		vtr = vtr > vmax? vmax : vtr;
+		vtr = vtr < vmin? vmin : vtr;
+   		
+		double sigmd = (vtr <= 0? vtr/(1.0*vmin) : vtr/(1.0*vmax));
+		double u = std::rand()/(1.0*INT_MAX);
+
+		if(u > sigmd) {
+			vtr = ceil(vtr);	
+	
+		} else {
+			vtr = floor(vtr);	
+		}
+					
+	}
 }
 
 double Particle::calculateObjectiveFn() const {
-	double sumFcost = 0.0,sumFlowCost=0.0;
+	double sumFcost = 0.0, sumFlowCost=0.0;
+	double alpha = _param.getAlpha();
 	std::set<int> setOfHubs(current_solution.begin(),current_solution.end());
+    
+	// sum fi*yi, for all i 	
 	for(const unsigned& i : setOfHubs)
 		sumFcost += _param.getFi(i);
 
+	// Sum of flow cost in case of AP instances
 	if(_param.format == FI_AP) {
 		double c = _param.getC();
 		double d = _param.getD();
-		double cik,ckm,cmj;
 
 		for (int i = 0; i < _param.getN() ; ++i) 
 			for (int j = 0;  j < _param.getN() ; ++j)  {
 				if(i!=j) {
-					cik = c*calculateFlowCost(i,current_solution[i]);
-					ckm =	_param.getAlpha() *calculateFlowCost(current_solution[i],
-							current_solution[j]);
-					cmj = d*calculateFlowCost(current_solution[j],j);
+					//(c*cik+alpha*ckm+d*cmj)*wij
+					double cik,ckm,cmj,wij;
+					wij = _param.getWij(i,j);
+					cik = c*_param.getCij(i,current_solution[i])*wij;
+					ckm =	alpha *_param.getCij(current_solution[i],
+							current_solution[j])*wij;
+					cmj = d*_param.getCij(current_solution[j],j)*wij;
 					sumFlowCost += cik+ckm+cmj;
 				}
 
 			}
 	} else {
-		double sumFlowCost = 0.0;
-		double c1,c2,c3;
-
+	// Sum of flow cost in case of CAB or RAND instances
 		for(int i = 0; i < _param.getN() ; ++i)
 			for(int j = i; j < _param.getN() ; ++j) {
 				if(i != j) {
-					c1 = calculateFlowCost(i,current_solution[i]);
-					c2 = _param.getAlpha()*calculateFlowCost(current_solution[i],
-				                           current_solution[j]);
-					c3 = calculateFlowCost(current_solution[j],j);			
-					sumFlowCost += 2 * (c1 + c2 + c3);
+					//(cik+ckm+cmj)*wij + (cjm+cmk+cki)*wji
+					double cik,ckm,cmj,wij;
+					wij = _param.getWij(i,j);
+					cik = _param.getCij(i,current_solution[i])*wij;
+					ckm = _param.getCij(current_solution[i],
+							current_solution[j])*wij*alpha;
+					cmj = _param.getCij(current_solution[j],j)*wij;
+					sumFlowCost += 2 * (cik + ckm + cmj);
 				}
 			}
 	}
+
 	return sumFlowCost+sumFcost;
 }
 
